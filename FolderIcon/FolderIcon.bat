5e7296202efdac242ea4229858a19e8b0aa279ab
aminkvh <55192322+aminkvh@users.noreply.github.com>
2020-09-15 14:08:32 +0430

    Update FolderIcon.bat

--- FolderIcon/FolderIcon.bat
+++ FolderIcon/FolderIcon.bat
@@ -5,7 +5,7 @@ echo [.ShellClassInfo] > %1\Desktop.ini
 echo IconFile=FolderIcon.ico >> %1\Desktop.ini
 echo IconIndex=0 >> %1\Desktop.ini
 for %%f in (%1\*.ico) do (
-    if "%%~xf"==".ico" ren "%%f" "FolderIcon.ico" | attrib +h +s %1\"FolderIcon.ico"
+    if "%%~xf"==".ico" ren "%%f" "FolderIcon.ico" & attrib +h +s %1\"FolderIcon.ico"
 )
 attrib +S +H %1\desktop.ini
 attrib +R +S %1
